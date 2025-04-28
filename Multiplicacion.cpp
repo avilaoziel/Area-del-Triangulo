1b36d2c0cc807df5cbf3360f7e4a5d39b632a4ab
avilaoziel <oziavilague@gmail.com>
2025-04-28 09:01:54 -0600

    Add files via upload

---
+++ Multiplicacion.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1,num2,resul;
+cout<<"bienvenido a multiplicacion\n";
+cout<<"porfavor intruduzca los numeros a multiplicar\n";
+cin>>num1;
+cout<<"\n";
+cin>>num2;
+resul=num1*num2;
+cout<<"el resultado fue\n";
+cout<<resul;
+
+}
