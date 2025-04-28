21c797c079a8180114a54bf1282a8f1a06121fc2
avilaoziel <oziavilague@gmail.com>
2025-04-28 08:57:33 -0600

    Add files via upload

---
+++ Area del circulo.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int radio,pi,resul;
+cout<<"bienvenido a area del circulo\n";
+cout<<"porfavor intruduzca el radio\n";
+cin>>radio;
+pi=3.1416;
+resul=radio*radio*pi;
+cout<<"el resultado fue\n";
+cout<<resul;
+
+}
---
+++ resta.cpp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+float r;
+cin>>a;
+cin>>b;
+r=a-b;
+cout<<r;
+    return 0;
+} 
